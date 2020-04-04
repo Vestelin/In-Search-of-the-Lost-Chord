f38ca31424728c1e1f69fe6578384657c595bf28
V <ytowar@gmail.com>
2020-04-04 12:40:42 +0200

    init commit

---
+++ android/app/src/main/kotlin/com/example/in_search_of_the_lost_chord/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.in_search_of_the_lost_chord
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
