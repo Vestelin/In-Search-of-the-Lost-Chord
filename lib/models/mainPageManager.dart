7166ab1efefcc4203f3e5e0a4ddfd40b1a11759e
V <ytowar@gmail.com>
2020-05-12 19:44:23 +0200

    fix

--- lib/models/mainPageManager.dart
+++ lib/models/mainPageManager.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:in_search_of_the_lost_chord/dataManagement/database.dart';
-import 'package:in_search_of_the_lost_chord/models/ratingAnimatedListCore.dart';
-import 'package:in_search_of_the_lost_chord/models/release.dart';
-import 'package:in_search_of_the_lost_chord/widgets/inherited/ratingListCoreProvider.dart';
-import 'package:in_search_of_the_lost_chord/widgets/lesser/releaseTile.dart';
 import 'package:in_search_of_the_lost_chord/widgets/lesser/showReleaseList.dart';
-import 'package:in_search_of_the_lost_chord/widgets/mainPageTabs.dart';
 
 class MainPageManager {
   int selected;
@@ -18,7 +12,7 @@ class MainPageManager {
   Widget getProperBody() {
     switch(selected) {
       case 0:
-        return ShowReleaseList();//RatingListCoreProvider(RatingAnimatedListCore<Release>((item) => ReleaseTile(item, ObjectKey(item)), GlobalKey<AnimatedListState>(), Database.releases, true), child: ShowReleaseList());
+        return ShowReleaseList();
       case 1:
         return Icon(Icons.camera);
       default:
